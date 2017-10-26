9bbe30af54af32d4fcbd553ffccc0539d73fbd9c
isaac72.choi <isaac72.choi@partner.samsung.com>
2017-10-26 16:46:01 +0900

    강좌2

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
-    render(){
+	sayHey(){
+		alert("hey");
+	}
 
-        return (
-                <h1>Hello React Skeleton</h1>
-        );
-    }
+	render(){
+			let text = "Dev-server";
+			let pStyle={
+				color:'aqua',
+				backgroundColor:'black'
+			};
+
+			return (
+					<div>
+						<h1>Hello React Skeleton</h1>
+						<h2>Welcome to {text}</h2>
+						<button onClick={this.sayHey}>Click Me</button>
+						<p style={pStyle}>{1 === 1 ? 'True' : 'False'}</p>
+					</div>
+			);
+	}
 }
 
 export default App;
